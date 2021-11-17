b9721590d6e88ca22352ebabc24e53b4ef17389b
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2021-11-17 13:00:27 +0900

    Add files via upload

--- streamlit2_app.py
+++ streamlit2_app.py
@@ -52,3 +52,11 @@ filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 
 st.subheader('Map of all pickups at %s:00' % hour_to_filter)
 st.map(filtered_data)
+
+
+
+
+st.subheader('이름을 바꿔줌111 맨위에 히스토그램')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+# 히스토 그램 넣는 방법
