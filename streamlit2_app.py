af1570ccd528919b58681ff862ca1dff73459016
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2021-11-17 13:12:39 +0900

    Add files via upload

--- streamlit2_app.py
+++ streamlit2_app.py
@@ -72,3 +72,34 @@ st.subheader('이름을 바꿔줌111 맨위에 히스토그램')
 hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
 # 히스토 그램 넣는 방법
+
+
+st.subheader('이름을 바꿔줌111 맨위에 히스토그램')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+# 히스토 그램 넣는 방법
+
+
+st.subheader('이름을 바꿔줌111 맨위에 히스토그램')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+# 히스토 그램 넣는 방법
+
+
+st.subheader('이름을 바꿔줌111 맨위에 히스토그램')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+# 히스토 그램 넣는 방법
+
+
+st.subheader('이름을 바꿔줌111 맨위에 히스토그램')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+# 히스토 그램 넣는 방법
+
+
+
+st.subheader('이름을 바꿔줌111 맨위에 히스토그램')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+# 히스토 그램 넣는 방법
