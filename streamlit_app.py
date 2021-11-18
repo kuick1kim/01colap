873a5f410bc0373fab9ae23c5683e932cd5e8fef
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2021-11-18 11:47:47 +0900

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,13 +59,13 @@ st.write('''
 # https://finance.yahoo.com/quote/005930.KS?p=005930.KS
 dr = pdr.get_data_yahoo('005930.KS',start_date,end_date)
 # dr=pd.DataFrame(dr)
-# st.line_chart(dr.Close)
-# st.line_chart(dr.Volume)
+st.line_chart(dr.Close)
+st.line_chart(dr.Volume)
 
 
 
-fig_close1 = px.line(dr, x=index, y=['Open', 'High', 'Low', 'Close'], title='가격')
-# fig_volume1 = px.line(dr, x='Date', y=['Volume'], title='Volume')
+# fig_close1 = px.line(dr, x=index, y=['Open', 'High', 'Low', 'Close'], title='가격')
+# # fig_volume1 = px.line(dr, x='Date', y=['Volume'], title='Volume')
 
-st.plotly_chart(fig_close1)
+# st.plotly_chart(fig_close1)
 # st.plotly_chart(fig_volume1)
