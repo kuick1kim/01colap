c3cb619a63bb65d2ec860f1390ed9a072c82e388
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2021-11-18 11:03:43 +0900

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,42 @@
+import cv2
+from tensorflow.keras.models import load_model
 import streamlit as st
-import time
+from streamlit_drawable_canvas import st_canvas
 import numpy as np
 
-progress_bar = st.sidebar.progress(0)
-status_text = st.sidebar.empty()
-last_rows = np.random.randn(1, 1)
-chart = st.line_chart(last_rows)
+@st.cache(allow_output_mutation=True)
+def load():
+    return load_model('model.h5')
+model = load()
 
-for i in range(1, 101):
-    new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
-    status_text.text("%i%% Complete" % i)
-    chart.add_rows(new_rows)
-    progress_bar.progress(i)
-    last_rows = new_rows
-    time.sleep(0.05)
+st.write('# MNIST Recognizer')
 
-progress_bar.empty()
+CANVAS_SIZE = 192
 
-# Streamlit widgets automatically run the script from top to bottom. Since
-# this button is not connected to any other logic, it just causes a plain
-# rerun.
-st.button("Re-run")
+col1, col2 = st.beta_columns(2)
+
+with col1:
+    canvas = st_canvas(
+        fill_color='#000000',
+        stroke_width=20,
+        stroke_color='#FFFFFF',
+        background_color='#000000',
+        width=CANVAS_SIZE,
+        height=CANVAS_SIZE,
+        drawing_mode='freedraw',
+        key='canvas'
+    )
+
+if canvas.image_data is not None:
+    img = canvas.image_data.astype(np.uint8)
+    img = cv2.resize(img, dsize=(28, 28))
+    preview_img = cv2.resize(img, dsize=(CANVAS_SIZE, CANVAS_SIZE), interpolation=cv2.INTER_NEAREST)
+
+    col2.image(preview_img)
+
+    x = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    x = x.reshape((-1, 28, 28, 1))
+    y = model.predict(x).squeeze()
+
+    st.write('## Result: %d' % np.argmax(y))
+    st.bar_chart(y)
