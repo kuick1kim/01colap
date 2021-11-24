6072ee845c26b9b1354a6db3da16d5ef5ed2b2ae
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2021-11-24 16:33:04 +0900

    Update bible.py

--- streamlit/bible.py
+++ streamlit/bible.py
@@ -14,19 +14,17 @@ def load_data():
 df = load_data()
 
 
-# df= pd.read_csv('c:/123kms/성경전체.csv')
 
-jang = st.text_input("특정 성경을 넣어주세요/ 안넣으시면 전체 검색 입니다. ",'누가복음')
+word = st.text_input("검색하고 싶은 말씀을 넣어주세요",'엘리사')
+df5 = df[df["말씀"].str.contains(word)].copy()
 
+bible = st.text_input("성경을 넣어주세요/(백스페이스 아래 '|' 마크를 넣으시면 중복 검색됩니다. ) ",'열왕기')
+df4 = df5[df5["성경"].str.contains(bible)].copy()
 
-df4 = df[df["성경"].str.contains(jang)].copy()
+st.write(df4) 
 
-word = st.text_input("검색하고 싶은 말씀을 넣어주세요",'사랑')
-df5 = df4[df4["말씀"].str.contains(word)].copy()
-st.write(df5) 
-
-for kk , ll ,pp,oo in zip(df5['말씀'], df5['성경'], df5['장'], df5['절']):
+for kk , ll ,pp,oo in zip(df4['말씀'], df4['성경'], df4['장'], df4['절']):
     aa= str(ll) +" "+str(pp)+'-'+str(oo)
-    st.write(aa)
-    st.write(kk)
+    st.caption(aa)
+    st.caption(kk)
     
