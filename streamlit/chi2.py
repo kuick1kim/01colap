7dd2ef355e0f356b1ddffd90f897e481a707f0f9
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2021-11-28 21:18:25 +0900

    Update chi2.py

--- streamlit/chi2.py
+++ streamlit/chi2.py
@@ -66,8 +66,8 @@ freq = df.groupby(['time']).count()
 freq = freq.reset_index(inplace=False)
 freq = freq[['time','지역']].sort_values(by='time',ascending=1)
 st.write(alt.Chart(freq).mark_bar().mark_area(
-    color="red",
-    fill="red",
+    color="green",
+    fill="green",
     interpolate='step-after'
 ).encode(
     x=alt.X('time', sort='-x'),
