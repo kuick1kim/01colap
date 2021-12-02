94bded77ad4dcdd8e86c8ab1e8566caa4e62252c
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2021-12-03 07:57:32 +0900

    Update bible.py

--- streamlit/bible.py
+++ streamlit/bible.py
@@ -18,16 +18,16 @@ df = load_data()
 
 
 word = st.text_input("검색하고 싶은 말씀을 넣어주세요",'엘리사')
-df5 = df[df["말씀"].str.contains(word)].copy()
+df1 = df[df["말씀"].str.contains(word)].copy()
 
 bible = st.text_input("성경을 넣어주세요/(백스페이스 아래 '|' 마크를 넣으시면 중복 검색됩니다. ) ",'열왕기')
-df4 = df5[df5["성경"].str.contains(bible)].copy()
+df2 = df1[df1["성경"].str.contains(bible)].copy()
 
 bible1 = st.text_input("장수를 넣어주세요")
-df6 = df5[df5["장"]=='bible1'].copy()
-st.write(df6) 
+df3 = df2[df2["장"]=='bible1'].copy()
+st.write(df3) 
 
-for kk , ll ,pp,oo in zip(df6['말씀'], df6['성경'], df6['장'], df6['절']):
+for kk , ll ,pp,oo in zip(df3['말씀'], df3['성경'], df3['장'], df3['절']):
     aa= str(ll) +" "+str(pp)+'-'+str(oo)
     st.write(aa)
     st.write(kk)
