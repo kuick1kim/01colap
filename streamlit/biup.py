591d72d86fd4a6c58b5dca65afd40ffd44d4c3f6
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2021-12-16 18:02:55 +0900

    Update biup.py

--- streamlit/biup.py
+++ streamlit/biup.py
@@ -13,7 +13,10 @@ def load_data():
     return data
 df = load_data()
 # i="https://raw.githubusercontent.com/kuick1kim/01colap/main/streamlit/UiPath.png"
-st.image('https://www.uipath.com/hubfs/resources/logos/UiPath-logo-orange-og-image.png')
+
+st.markdown("![Foo](http://www.google.com.au/images/nav_logo7.png)(http://google.com.au/)")
+
+# st.image('https://www.uipath.com/hubfs/resources/logos/UiPath-logo-orange-og-image.png')
 try:
     st.image(i)
 except:
