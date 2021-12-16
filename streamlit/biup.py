48e6bf36ee3006b5709bd07f392ddafc87123bb1
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2021-12-16 18:08:58 +0900

    Update biup.py

--- streamlit/biup.py
+++ streamlit/biup.py
@@ -12,15 +12,10 @@ def load_data():
     data = pd.read_csv(DATA_URL)    
     return data
 df = load_data()
-# i="https://raw.githubusercontent.com/kuick1kim/01colap/main/streamlit/UiPath.png"
 
-st.markdown("![Foo](https://www.uipath.com/hubfs/resources/logos/UiPath-logo-orange-og-image.png)")
+st.markdown("![Foo](https://www.epicloud.com.tw/wp-content/uploads/2020/09/%E6%9C%AA%E5%91%BD%E5%90%8D%E8%A8%AD%E8%A8%88-14.png)")
 
-# st.image('https://www.uipath.com/hubfs/resources/logos/UiPath-logo-orange-og-image.png')
-try:
-    st.image(i)
-except:
-    pass      
+
 st.write(df) 
 akiml= len(df)
 many = st.sidebar.slider('몇번째 회사인지 선택해 주세요',  0, akiml-1, 0)
