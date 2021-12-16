545372cbe185af18ee12d468bdc8ad394d8f2f1b
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2021-12-17 08:08:55 +0900

    Update biup.py

--- streamlit/biup.py
+++ streamlit/biup.py
@@ -17,6 +17,18 @@ df = load_data()
 
 
 st.write(df) 
+##########################################################################
+towrite = io.BytesIO()
+downloaded_file = df.to_excel(towrite, encoding='utf-8', index=True, header=True)
+towrite.seek(0)  # reset pointer
+b64 = base64.b64encode(towrite.read()).decode()  # some strings
+linko= f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="전체보기.xlsx">자료 다운받기</a>'
+st.markdown(linko, unsafe_allow_html=True)
+#===============================인코딩이 안되서 csv를 안씀================================
+
+
+
+
 akiml= len(df)
 many = st.slider('몇번째 회사인지 선택해 주세요',  0, akiml-1, 0)
 
