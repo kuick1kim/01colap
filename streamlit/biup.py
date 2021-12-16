2779e527a983546b11f94da5de891078bcf7ac3d
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2021-12-16 17:26:12 +0900

    Update biup.py

--- streamlit/biup.py
+++ streamlit/biup.py
@@ -29,7 +29,7 @@ df2= df1['경험담k.1']
 df2= df2.split("#####")
 for a in df2:
   st.header(a)
-  st.header()
+  st.write()
   
   
   
@@ -37,6 +37,7 @@ df3= df1['story_k.1']
 df3= df3.split("#####")
 for b in df3:
   st.write(b)
-  st.header()
-  st.header()
+  st.write()
+  st.write()
+  st.write()
 
