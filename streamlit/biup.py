e9bbc66ead1b6c9d0daf34935e0943e6c9d0a0d5
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2021-12-20 15:11:47 +0900

    Update biup.py

--- streamlit/biup.py
+++ streamlit/biup.py
@@ -26,8 +26,10 @@ n= len(df)
 # st.write(num) 
 
 
-age = st.slider('How old are you?', 0, 130, 25)
-st.write("I'm ", age, 'years old')
+values = st.slider(
+     'Select a range of values',
+     0.0, 100.0, (25.0, 75.0))
+st.write('Values:', values)
 
 
 dfa= df.iloc[3,:]
