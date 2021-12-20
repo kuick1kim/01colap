30a09491dc4bb58c80663ce9c04f60f724fd95cc
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2021-12-20 15:03:34 +0900

    Update biup.py

--- streamlit/biup.py
+++ streamlit/biup.py
@@ -12,41 +12,41 @@ def load_data():
     return data
 df = load_data()
 
-st.title("사이트가 변경되었습니다. 감사합니다.") 
+st.title("주 예수를 믿으라 그리하면 너와 네 집이 구원을 받으리라.") 
 st.write()
 st.write()
 st.write()
 st.write()
-# st.write(df) 
+st.write(df) 
 
 
-DATA_URL = ('https://raw.githubusercontent.com/kuick1kim/01colap/main/bible.csv')
-@st.cache
-def load_data():
-    data = pd.read_csv(DATA_URL)    
-    return data
-df = load_data()
+# DATA_URL = ('https://raw.githubusercontent.com/kuick1kim/01colap/main/bible.csv')
+# @st.cache
+# def load_data():
+#     data = pd.read_csv(DATA_URL)    
+#     return data
+# df = load_data()
 
 
 
-word = st.text_input("검색하고 싶은 성경 말씀을 넣어주세요",'주 예수를 믿으라 그리하면 너와 네 집이 구원을 받으리라 ')
-df1 = df[df["말씀"].str.contains(word)].copy()
+# word = st.text_input("검색하고 싶은 성경 말씀을 넣어주세요",'주 예수를 믿으라 그리하면 너와 네 집이 구원을 받으리라 ')
+# df1 = df[df["말씀"].str.contains(word)].copy()
 
-bible = st.text_input("성경을 넣어주세요/(백스페이스 아래 '|' 마크를 넣으시면 중복 검색됩니다. ) ",'')
-df2 = df1[df1["성경"].str.contains(bible)].copy()
+# bible = st.text_input("성경을 넣어주세요/(백스페이스 아래 '|' 마크를 넣으시면 중복 검색됩니다. ) ",'')
+# df2 = df1[df1["성경"].str.contains(bible)].copy()
 
-bible1 = st.text_input("장수를 넣어주세요")
+# bible1 = st.text_input("장수를 넣어주세요")
 
-if bible1 != "":    
-    bible1 = int(bible1)
-    df2 = df2[df2["장"]==bible1].copy()
+# if bible1 != "":    
+#     bible1 = int(bible1)
+#     df2 = df2[df2["장"]==bible1].copy()
 
     
-st.write(df2) 
+# st.write(df2) 
 
-for kk , ll ,pp,oo in zip(df2['말씀'], df2['성경'], df2['장'], df2['절']):
-    aa= str(ll) +" "+str(pp)+'-'+str(oo)
-    st.write(aa)
-    st.write(kk)
+# for kk , ll ,pp,oo in zip(df2['말씀'], df2['성경'], df2['장'], df2['절']):
+#     aa= str(ll) +" "+str(pp)+'-'+str(oo)
+#     st.write(aa)
+#     st.write(kk)
     
     
