b07891470ec51d7251415631f6723b59333408c9
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2021-12-26 18:45:01 +0900

    Update naver_news50save.py

--- streamlit/naver_news50save.py
+++ streamlit/naver_news50save.py
@@ -14,39 +14,39 @@ def gather1():
     headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36'}
     df = pd.DataFrame(columns=["날짜","언론사","제목","내용","네이버링크","기사링크","사진링크"])
     try:
-    for num in range(0,51,1):
-        
-        num1=num*10+1
+        for num in range(0,51,1):
 
-        news_url = 'https://search.naver.com/search.naver?where=news&query={}&sm=tab_opt&sort=0&start={}'.format(query,num1)
-        if num%10 ==0:
-            st.write(num)
-        req = requests.get(news_url, headers = headers)
-        soup = BeautifulSoup(req.text, "html.parser")
-        
-        ul= soup.find("ul","list_news")
-        lili= ul.find_all("li", "bx")
+            num1=num*10+1
 
-        for i in range(len(lili)):
-            li = ul.find_all("li","bx")[i]
-            dan1 = li.find("div", "info_group")
-            unlonsa = dan1.find("a").text
-            time = dan1.find("span","info").text
-            try:
-                potallink = dan1.find_all("a")[1]['href']
-            except:
-                potallink = "네이버 링크는 없음"
+            news_url = 'https://search.naver.com/search.naver?where=news&query={}&sm=tab_opt&sort=0&start={}'.format(query,num1)
+            if num%10 ==0:
+                st.write(num)
+            req = requests.get(news_url, headers = headers)
+            soup = BeautifulSoup(req.text, "html.parser")
 
-            link = li.find_all("a")[5]['href']
-            try:
-                img = li.find_all('img')[1]['src']
-            except:
-                img = "이미지는 없슴"
-            title = li.find('a','news_tit').text
-            article1 = li.find('div','news_dsc').text.replace("\n","")
-            gisa1 = re.compile(r'[^ A-Za-z0-9가-힣+]').sub('',article1)
-            df = df.append({"날짜":time,"언론사":unlonsa,"제목":title,"내용":gisa1,
-                            "네이버링크":potallink,"기사링크":link,"사진링크":img}, ignore_index=True)    
+            ul= soup.find("ul","list_news")
+            lili= ul.find_all("li", "bx")
+
+            for i in range(len(lili)):
+                li = ul.find_all("li","bx")[i]
+                dan1 = li.find("div", "info_group")
+                unlonsa = dan1.find("a").text
+                time = dan1.find("span","info").text
+                try:
+                    potallink = dan1.find_all("a")[1]['href']
+                except:
+                    potallink = "네이버 링크는 없음"
+
+                link = li.find_all("a")[5]['href']
+                try:
+                    img = li.find_all('img')[1]['src']
+                except:
+                    img = "이미지는 없슴"
+                title = li.find('a','news_tit').text
+                article1 = li.find('div','news_dsc').text.replace("\n","")
+                gisa1 = re.compile(r'[^ A-Za-z0-9가-힣+]').sub('',article1)
+                df = df.append({"날짜":time,"언론사":unlonsa,"제목":title,"내용":gisa1,
+                                "네이버링크":potallink,"기사링크":link,"사진링크":img}, ignore_index=True)    
     except:
         st.header("검색어를 넣어주세요")   
     df
