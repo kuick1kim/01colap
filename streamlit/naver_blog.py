98d081fbf201aaae249a82a15613d99841bee15a
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2021-12-26 23:46:59 +0900

    Update naver_blog.py

--- streamlit/naver_blog.py
+++ streamlit/naver_blog.py
@@ -14,6 +14,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 options = Options()
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
+
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
 options.add_argument("--disable-features=NetworkService")
