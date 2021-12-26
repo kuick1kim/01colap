c71dad54586ee49ce58242c0d83b93018c3e175f
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2021-12-26 19:27:20 +0900

    Update naver_news50save.py

--- streamlit/naver_news50save.py
+++ streamlit/naver_news50save.py
@@ -58,7 +58,7 @@ def gather1():
     linko= f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="[검색어]{qu}.xlsx">내가 검색한 자료 다운받기</a>'
     st.markdown(linko, unsafe_allow_html=True)
     
-    for h, i,j,k,kk in zip(df['언론사'], df['날짜'],df['제목'],df['링크'],df['내용']):
+    for h, i,j,k,kk in zip(df['언론사'], df['날짜'],df['제목'],df['기사링크'],df['내용']):
             st.write(i," = = ",h)
             st.write(j)
             st.write(k)
@@ -77,74 +77,3 @@ if __name__ == '__main__':
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def main():
-#     st.header('언론사별 주요뉴스 ')
-#     query = st.text_input('날짜를 넣어주세요.. 자료가 없을수도 있어요. ex)검색어 ')
-
-#     news_url = 'https://search.naver.com/search.naver?where=news&query={}&sm=tab_opt&sort=0&start=1'.format(query)
-
-#     headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36'}
-#     req = requests.get(news_url, headers = headers)
-
-#     soup = BeautifulSoup(req.text, 'html.parser')
-
-#     df = pd.DataFrame(columns=['언론사','기사','날짜','링크'])
-
-#     h111 = soup.select('.rankingnews_box')
-
-#     num = 0
-    
-#     for i in range(len(h111)):        
-#         h112=h111[i].find('strong').text
-#         lll= h111[i].find_all('div')
-#         kkk= h111[i].find_all('li')       
-        
-#         for j in range(len(lll)):
-#             aa = lll[j].find('span').text
-#             bb1 = lll[j].find('a').text
-#             cc = lll[j].find('a').get('href')
-#             num=num+1
-#             df=df.append({'언론사':h112,'날짜': aa,'기사':bb1,'링크':cc}, ignore_index=True)
-
-#     st.write(num)  
-
-#     ki = st.sidebar.slider( '기사', 0, num, (0, num))    
-#     df1 = df.loc[ki[0]:ki[1], :]
-
-#     sorted_unique_team = sorted(df1.언론사.unique())
-#     selected = st.sidebar.multiselect('Team', sorted_unique_team, sorted_unique_team)
-        
-#     df2 = df1[df1.언론사.isin(selected)]
-
-#     st.write(df2) 
-
-#     for h, i,j,k in zip(df2['언론사'], df2['날짜'],df2['기사'],df2['링크']):
-#         st.write(h," = = ",i,"=====",j,"<br>",k)
-        
-
-# if __name__ == '__main__':
-#     main()
