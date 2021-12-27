e49c78483762c76ed6f61eee74b0d025df8fb3ee
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2021-12-27 17:09:46 +0900

    Update newss.py

--- streamlit/newss.py
+++ streamlit/newss.py
@@ -10,7 +10,7 @@ st.sidebar.markdown("""
 
 ########################여기는 추가넣는부분##############
 
-
+st.title('블로그 분석기')
 
 uploaded_file = st.file_uploader("Choose a XLSX file", type="xlsx")
 
@@ -27,25 +27,25 @@ else:
 llll = len(df)
 
 
-many = st.slider('기간',  0, llll, (0, llll))
+many = st.sidebar.slider('기간',  0, llll, (0, llll))
 df1 = df.iloc[many[0]:many[1],:]
 
 
 
-# cate = st.text_input("검색하고 싶은 언론사를 넣어주세요",'')    
-# df3 = df1[df1["category"].str.contains(cate)].copy()
+cate1 = st.sidebar.text_input("분야1 넣어주세요",'')    
+df3 = df1[df1["분야1"].str.contains(cate1)].copy()
 
-df1
-# kiki = st.sidebar.text_input("검색하고 싶은 기사단어를 넣어주세요",'')    
-# df2 = df3[df3['기사'].str.contains(kiki)].copy()
+cate2 = st.sidebar.text_input("검색내용 넣어주세요",'')    
+df4 = df3[df3["내용"].str.contains(cate2)].copy()
+
+df4
 
 
-
-# for h, i,j,k,kk in zip(df3["날짜"], df3['category'],df3['제목'],df3['링크'],df3['내용']):
-#             st.write(i," = = ",h)
-#             st.write(j)
-#             st.write(k)
-#             st.write(kk)
+for h,i,j,k,kk,kkk in zip(df4["날짜"], df4['블로거'],df4['분야1'],df4['제목'],df4['링크'],df4['내용']):
+            st.write(i," = = ",h," = = ",j)
+            st.write(k)
+            st.write(kk)
+            st.write(kkk)
 
 
 
