0955a19b5775170cc7398485d501a744b3c5b853
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2022-01-22 13:21:00 +0900

    Update n_blog.py

--- streamlit/n_blog.py
+++ streamlit/n_blog.py
@@ -32,16 +32,15 @@ list = st.sidebar.slider( '선택하세요',0, len(df)-1, 5)
 st.write(list)
 df1 = df.iloc[list:list+1, :]
 
-dfs = pd.DataFrame(columns=['내용'])
 
 for i in df1['내용']:
     hh1=i.split('\t')
     a=1
     for nn in hh1:
         if nn is not None or nn != " " or nn != "" :
-            nn1= nn+'//'+str(a)            
+            nn1= nn+'//'+str(a)        
+            st.write(nn1)
             a=a+1
-            dfs=dfs.append({'내용':nn1}, ignore_index=True)
 
     
 
