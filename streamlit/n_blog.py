34154353633cd30a870abfd0e1d50a57cc040a56
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2022-01-22 14:06:56 +0900

    Update n_blog.py

--- streamlit/n_blog.py
+++ streamlit/n_blog.py
@@ -12,7 +12,7 @@ def load_data(name):
     data = None
     if name == '블로그 검색1':
         DATA_URL= kms()        
-        data = pd.read_csv(DATA_URL)   
+        
     elif name == '블로그 검색2':
         DATA_URL='https://raw.githubusercontent.com/kuick1kim/01colap/main/csv/blog2.xlsx'
         data = pd.read_csv(DATA_URL)  
@@ -22,32 +22,49 @@ def load_data(name):
     return data
     
 def kms():
-    a = 'https://raw.githubusercontent.com/kuick1kim/01colap/main/csv/blog2.csv'           
-    return a
-
-df = load_data(dataset_name)
-
-spectra = st.file_uploader(" ", type={"csv", "txt", "xlsx"})
-if spectra is not None:
-    df = pd.read_csv(spectra)
-#     dataset_name='외부데이터' 
-
+    a = 'https://raw.githubusercontent.com/kuick1kim/01colap/main/csv/blog2.csv'  
+    df = pd.read_csv(a)  
+    spectra = st.file_uploader(" ", type={"csv", "txt", "xlsx"})
+    if spectra is not None:
+        df = pd.read_csv(spectra)
+    list = st.sidebar.slider( '선택하세요',0, len(df)-1, 5)
+    st.write(list)
+    df1 = df.iloc[list:list+1, :]
 
 
+    for i,j in zip(df1['내용'],df1['링크']):
+        st.write(j)
+        hh1=i.split('\t')
+        a=1
+        for nn in hh1:
+            if nn is not None or nn != " " or nn != "" :                
+                st.write(nn)
+                a=a+1
+    
 
-list = st.sidebar.slider( '선택하세요',0, len(df)-1, 5)
-st.write(list)
-df1 = df.iloc[list:list+1, :]
+# df = load_data(dataset_name)
+
+# spectra = st.file_uploader(" ", type={"csv", "txt", "xlsx"})
+# if spectra is not None:
+#     df = pd.read_csv(spectra)
+# #     dataset_name='외부데이터' 
 
 
-for i,j in zip(df1['내용'],df1['링크']):
-    st.write(j)
-    hh1=i.split('\t')
-    a=1
-    for nn in hh1:
-        if nn is not None or nn != " " or nn != "" :                
-            st.write(nn)
-            a=a+1
+
+
+# list = st.sidebar.slider( '선택하세요',0, len(df)-1, 5)
+# st.write(list)
+# df1 = df.iloc[list:list+1, :]
+
+
+# for i,j in zip(df1['내용'],df1['링크']):
+#     st.write(j)
+#     hh1=i.split('\t')
+#     a=1
+#     for nn in hh1:
+#         if nn is not None or nn != " " or nn != "" :                
+#             st.write(nn)
+#             a=a+1
          
 
     
