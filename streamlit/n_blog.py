9b3a05e03c379dfdad664c8c00b87054a13a10f9
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2022-01-22 14:39:49 +0900

    Update n_blog.py

--- streamlit/n_blog.py
+++ streamlit/n_blog.py
@@ -87,13 +87,7 @@ def kms():
     except:
         st.header("검색어를 넣어주세요")   
     
-    towrite = io.BytesIO()
-    downloaded_file = df.to_excel(towrite, encoding='utf-8', index=True, header=True)
-    towrite.seek(0)  # reset pointer
-    b64 = base64.b64encode(towrite.read()).decode()
-    qu=query  # some strings
-    linko= f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="[검색어]{qu}.xlsx">내가 검색한 자료 다운받기</a>'
-    st.markdown(linko, unsafe_allow_html=True)
+    
     
     df
     
