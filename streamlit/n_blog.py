37ff737f6c7e7f211d3d6f46edb201fe3fabc3cc
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2022-01-22 20:13:44 +0900

    Update n_blog.py

--- streamlit/n_blog.py
+++ streamlit/n_blog.py
@@ -32,7 +32,7 @@ options.add_argument("--disable-features=VizDisplayCompositor")
 
 dataset_name = st.sidebar.selectbox(
     'Select Dataset',
-    ('큰검색','세부검색', '검색기')
+    ('큰검색','세부검색', '검색기S',"검색기B")
 )
 
 def get_dataset(name):
@@ -40,8 +40,10 @@ def get_dataset(name):
         ranking()
     elif name == '세부검색':
         ranking3() 
-    else:
+    elif name == '검색기S':
         blog_main()
+    else:
+        blog_main2()
 
 
 ######################################################################################################
@@ -54,7 +56,10 @@ def ranking():
     ########################여기는 추가넣는부분##############
     spectra = st.file_uploader(" ", type={"csv", "txt", "xlsx"})
     if spectra is not None:
-        df = pd.read_csv(spectra)
+        try:
+            df = pd.read_csv(spectra)
+        except:
+            df = pd.read_excel(spectra)
         dataset_name='외부데이터' 
 #     df 
     list = st.sidebar.slider( '여러가지 검색해 보세요', 0, len(df)-1, 5)
@@ -80,7 +85,11 @@ def ranking3():
     ########################여기는 추가넣는부분##############
     spectra = st.file_uploader(" ", type={"csv", "txt", "xlsx"})
     if spectra is not None:
-        df1 = pd.read_csv(spectra)
+        try:
+            df1 = pd.read_csv(spectra)
+        except:
+            df1 = pd.read_excel(spectra)
+        
         dataset_name='외부데이터' 
     
     word = st.sidebar.text_input("검색하고 싶은 내용검색/ 아무것도 안넣으면, 엄청돌아감",'마시고')
@@ -251,9 +260,83 @@ def blog_main():
     
     st.info('Successful finished. Selenium log file is shown below...')
 
-##################################################################################
+
+######################################################################################################
+def blog_main2():
+    oo=1
+    st.header('언론사별 주요뉴스 ')
+    session = requests.Session()
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit 537.36 (KHTML, like Gecko) Chrome",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+    }
+    spectra = st.file_uploader(" ", type={"csv", "txt", "xlsx"})
+    if spectra is not None:
+        try:
+            ddf = pd.read_csv(spectra)
+        except:
+            ddf = pd.read_excel(spectra)
+        dataset_name='외부데이터' 
+       
+    df = pd.DataFrame(columns=['작성자','시간','제목','링크','내용','이미지모음'])
+    dfs = pd.DataFrame(columns=['내용'])
     
-    
+    # query= input('내용을 쓰시요')
+    for ll in ddf['링크']:
+        if oo%10==0:
+            st.write(oo,"/전체는 ",len(ddf),'입니다')
+        oo=oo+1
+        a=ll.split('?')[0]
+        a=a.split("/")
+        b,c=a[-2],a[-1]
+
+        url= 'https://blog.naver.com/PostView.naver?blogId={}&logNo={}&redirect=Dlog&widgetTypeCall=true&directAccess=false'.format(b,c)
+
+        html = session.get(url, headers=headers).content
+        soup = BeautifulSoup(html, "html.parser")
+        try:
+
+            table = soup.find('table',id='printPost1')
+
+            title1 = table.find('div','pcol1').text
+            title= title1.replace('\n','') 
+
+            date = table.find('span','se_publishDate pcol2').text
+            name= table.find('span','nick').text
+
+            imgimg = table.find_all('img','se-image-resource')
+            imgb=''
+            for a in range(len(imgimg)):
+                img = table.find_all('img','se-image-resource')[a].get('data-lazy-src')    
+                imgb=imgb+"@@@@@"+str(img)    
+
+            storya= table.find_all('p',"se-text-paragraph")                            
+            storyb=""
+            for b in range(len(storya)):
+                story = table.find_all('p',"se-text-paragraph") [b].text
+                if story != "" :
+                    dfs=dfs.append({'내용':storyb}, ignore_index=True)
+                storyb = storyb+'\t'+str(story)
+                
+            df=df.append({'작성자':name,'시간':date,'제목':title,'링크':ll,'내용':storyb,
+                  '이미지모음':imgb}, ignore_index=True)
+                
+        except:
+            pass
+
+    towrite1 = io.BytesIO()
+    downloaded_file = df.to_excel(towrite, encoding='utf-8', index=False, header=True)
+    towrite1.seek(0)  # reset pointer
+    b64a = base64.b64encode(towrite1.read()).decode()    
+    linko1= f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64a}" download="[블로그]전체.xlsx">전체 자료 다운</a>'
+    st.markdown(linko1, unsafe_allow_html=True)
+
+    towrite2 = io.BytesIO()
+    downloaded_file = dfs.to_excel(towrite, encoding='utf-8', index=False, header=True)
+    towrite2.seek(0)  # reset pointer
+    b64b = base64.b64encode(towrite2.read()).decode()    
+    linko2= f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64b}" download="[블로그]내용만.xlsx">내용 자료 다운</a>'
+    st.markdown(linko2, unsafe_allow_html=True)
     
 
 
