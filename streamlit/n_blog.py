914f28069bc8f76cf5077a886a0be39696c032b0
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2022-01-22 15:12:59 +0900

    Update n_blog.py

--- streamlit/n_blog.py
+++ streamlit/n_blog.py
@@ -39,20 +39,18 @@ def ranking():
     
     
 ######################################################################################################
-
 def ranking2():
     st.header('언론사별 주요뉴스 ')
-    DATA_URL = ('https://raw.githubusercontent.com/kuick1kim/01colap/main/csv/blog3.xlsx')
-    df = pd.read_excel(DATA_URL,engine='openpyxl')  
-     
+    DATA_URL = ('https://raw.githubusercontent.com/kuick1kim/01colap/main/csv/blog3.csv')
+    df = pd.read_csv(DATA_URL)  
+    
     
     ########################여기는 추가넣는부분##############
     spectra = st.file_uploader(" ", type={"csv", "txt", "xlsx"})
     if spectra is not None:
-        df = pd.read_excel(spectra, engine='openpyxl' )
+        df = pd.read_csv(spectra)
         dataset_name='외부데이터' 
     df 
-    
     
 
 ##################################################################################
