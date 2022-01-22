972428e31653b727fa106b56586362c2747f4b73
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2022-01-22 15:06:06 +0900

    Update n_blog.py

--- streamlit/n_blog.py
+++ streamlit/n_blog.py
@@ -7,9 +7,7 @@ import io
 
 
 
-st.sidebar.markdown("""
-[뉴스 분석 사이트로 이동](https://share.streamlit.io/kuick1kim/01colap/main/streamlit/newss.py/)
-""")
+
 dataset_name = st.sidebar.selectbox(
     'Select Dataset',
     ('ranking','news', 'blog')
@@ -44,21 +42,20 @@ def ranking():
 
 def ranking2():
     st.header('언론사별 주요뉴스 ')
-    DATA_URL = ('https://raw.githubusercontent.com/kuick1kim/01colap/main/csv/blog3.csv')
-    df = pd.read_csv(DATA_URL)  
+    DATA_URL = ('https://raw.githubusercontent.com/kuick1kim/01colap/main/csv/blog3.xlsx')
+    df = pd.read_excel(DATA_URL)  
     
     
     ########################여기는 추가넣는부분##############
     spectra = st.file_uploader(" ", type={"csv", "txt", "xlsx"})
     if spectra is not None:
-        df = pd.read_csv(spectra)
+        df = pd.read_excel(spectra)
         dataset_name='외부데이터' 
     df 
     
     
 
 ##################################################################################
-
 
 if __name__ == '__main__':
     get_dataset(dataset_name)
