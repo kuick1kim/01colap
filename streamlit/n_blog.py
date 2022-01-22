6f45bc0881aa9bbd6c37a7778cdac09a78e6c2ea
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2022-01-22 10:46:17 +0900

    Add files via upload

---
+++ streamlit/n_blog.py
@@ -0,0 +1,70 @@
+
+
+
+import streamlit as st
+import pandas as pd
+
+
+
+
+########################여기는 추가넣는부분##############
+spectra = st.file_uploader(" ", type={"csv", "txt", "xlsx"})
+if spectra is not None:
+    try:
+        df = pd.read_excel(spectra)
+    except:
+        df = pd.read_csv(spectra)
+    dataset_name='외부데이터' 
+else :
+    df = "데이터를 넣어주세요"
+#########################성공###########################
+
+
+
+
+
+
+
+
+
+
+
+
+
+# DATA_URL = ('https://raw.githubusercontent.com/kuick1kim/01colap/main/bible.csv')
+# @st.cache
+# def load_data():
+#     data = pd.read_csv(DATA_URL)    
+#     return data
+# df = load_data()
+
+
+
+
+
+
+
+# # df= pd.read_csv('c:/123kms/성경전체.csv')
+
+
+# word = st.text_input("검색하고 싶은 말씀을 넣어주세요",'엘리사')
+# df5 = df[df["말씀"].str.contains(word)].copy()
+
+
+
+# bible = st.text_input("성경을 넣어주세요/(백스페이스 아래 '|' 마크를 넣으시면 중복 검색됩니다. ) ",'열왕기')
+# df4 = df5[df5["성경"].str.contains(bible)].copy()
+
+# # jang = st.text_input("특정 장수가 보고 싶으시면 넣어주세요")
+# # df44 = df4[df4["장"].str.count(jang)].copy()
+# # df44
+
+
+
+# st.write(df4) 
+
+# for kk , ll ,pp,oo in zip(df4['말씀'], df4['성경'], df4['장'], df4['절']):
+#     aa= str(ll) +" "+str(pp)+'-'+str(oo)
+#     st.write(aa)
+#     st.write(kk)
+    
