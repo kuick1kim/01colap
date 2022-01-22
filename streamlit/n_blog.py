b4f38f31fffed133d1b3a5fa318749e037ddcb52
kuick1kim <82426843+kuick1kim@users.noreply.github.com>
2022-01-22 13:07:41 +0900

    Update n_blog.py

--- streamlit/n_blog.py
+++ streamlit/n_blog.py
@@ -20,23 +20,15 @@ def load_data(name):
     data = pd.read_csv(DATA_URL)   
     return data
     
-    
-    
 def kms():
     a =  'https://raw.githubusercontent.com/kuick1kim/01colap/main/csv/blog2.csv'       
     return a
 
 df = load_data(dataset_name)
 
-
 list = st.sidebar.slider( '선택하세요',0, len(df)-1, 5)
 st.write(list)
 df1 = df.iloc[list:list+1, :]
-
-
-
-
-
 
 dfs = pd.DataFrame(columns=['내용'])
 
@@ -45,13 +37,12 @@ for i in df1['내용']:
     a=1
     for nn in hh1:
         if nn is not None or nn != " " or nn != "" :
-            nn1= nn+'//'+str(a)
-            
+            nn1= nn+'//'+str(a)            
             a=a+1
             dfs=dfs.append({'내용':nn1}, ignore_index=True)
 
     
-word = st.text_input("검색하고 싶은 단어를 넣으세요",'')
+word = st.sidebar.text_input("검색하고 싶은 단어를 넣으세요",'')
 dfs2 = dfs[dfs["내용"].str.contains(word)].copy()
 dfs2
 for i in dfs2:
